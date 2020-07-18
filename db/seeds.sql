406534e26adc711f03793ea43c3a707f0d26fff1
Lam Nguyen <lamcnguyen89@gmail.com>
2020-07-18 01:24:06 -0400

    changed some stuff

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,9 @@
 INSERT INTO burgers (name) VALUES ('Vegan Buddhist Burger');
 INSERT INTO burgers (name) VALUES ('Angel Tears Tiramisu & Triple Chocolate Travesty');
-INSERT INTO burgers (name) VALUES ('Tuna Sashimi Spicy Burger');
+INSERT INTO burgers (name) VALUES ('Tuna Sashimi Spicy Rice Burger');
+INSERT INTO burgers (name) VALUES ('Heart Attack Triple Cheese Organic Free Range Bacon Burger');
 INSERT INTO burgers (name, sleepy) VALUES ('Grassfed Beef Gargantuan Burger', true);
 INSERT INTO burgers (name, sleepy) VALUES ('Pontificating Pulled Pork Burger', true);
 INSERT INTO burgers (name, sleepy) VALUES ('Donald Trump Burger?', true);
-INSERT INTO burgers (name) VALUES ('Heart Attack Triple Cheese Organic Free Range Bacon Burger');
+INSERT INTO burgers (name, sleepy) VALUES ('Seared Salmon and Sauerkraut Burger', true);
+
