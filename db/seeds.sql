580c2256fce2ba6179dcabe8196004a293a4e0bc
Lam Nguyen <lamcnguyen89@gmail.com>
2020-07-17 22:06:31 -0400

    Built Handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
-INSERT INTO burgers (name) VALUES ('Meeses');
-INSERT INTO burgers (name) VALUES ('Bobbi');
-INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO burgers (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Vegan Buddhist Burger');
+INSERT INTO burgers (name) VALUES ('Angel Tears Tiramisu & Triple Chocolate Travesty');
+INSERT INTO burgers (name) VALUES ('Tuna Sashimi Spicy Burger');
+INSERT INTO burgers (name, sleepy) VALUES ('Grassfed Beef Gargantuan Burger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Pontificating Pulled Pork Burger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Donald Trump Burger?', true);
+INSERT INTO burgers (name) VALUES ('Heart Attack Triple Cheese Organic Free Range Bacon Burger');
